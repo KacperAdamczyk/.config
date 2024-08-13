c6b9cb84951cd883eef78ba982edf878fe83ea58
Kacper Adamczyk <kacper.k.adamczyk@gmail.com>
2024-08-14 00:07:02 +0200

    Change wezterm font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,7 +8,7 @@ local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
 config.color_scheme = "Dracula"
-config.font_size = 14.0
+config.font_size = 16.0
 
 -- and finally, return the configuration to wezterm
 return config
